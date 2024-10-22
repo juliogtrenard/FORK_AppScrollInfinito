4b9cfd6527f05bafe8a4351322ed2168c4056261
Julio <juliogtrenard@gmail.com>
2024-10-22 09:44:56 +0200

    ADD métodos para añadir tareas

--- app/src/main/java/es/juliogtrenard/miprimeraapp/MainActivity.kt
+++ app/src/main/java/es/juliogtrenard/miprimeraapp/MainActivity.kt
@@ -1,20 +1,38 @@
 package es.juliogtrenard.miprimeraapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btnAddTask:Button
+    private lateinit var etTask:EditText
+    private lateinit var rvTask:RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        initUI()
+    }
+
+    private fun initUI() {
+        initView()
+        initListeners()
+    }
+
+    private fun initListeners() {
+        btnAddTask.setOnClickListener {addTask()}
+    }
+
+    private fun addTask() {
+        val taskToAdd:String = etTask.text.toString()
+    }
+
+    private fun initView() {
+        btnAddTask = findViewById(R.id.btnAddTask)
+        etTask = findViewById(R.id.etTask)
+        rvTask = findViewById(R.id.rvTask)
     }
 }
