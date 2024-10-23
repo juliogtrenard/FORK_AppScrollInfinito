fff5d85df4e439c8ab09626f48846861641faddb
Julio <juliogtrenard@gmail.com>
2024-10-23 10:50:30 +0200

    Tareas se agregan correctamente

--- app/src/main/java/es/juliogtrenard/miprimeraapp/MainActivity.kt
+++ app/src/main/java/es/juliogtrenard/miprimeraapp/MainActivity.kt
@@ -1,9 +1,11 @@
 package es.juliogtrenard.miprimeraapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
@@ -11,11 +13,20 @@ class MainActivity : AppCompatActivity() {
     private lateinit var etTask:EditText
     private lateinit var rvTask:RecyclerView
     private var tasks = mutableListOf<String>()
+    private lateinit var adapter:TaskAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initUI()
+        initRecyclerView()
+    }
+
+    //Configura el RecyclerView para añadir el adapter
+    private fun initRecyclerView() {
+        rvTask.layoutManager = LinearLayoutManager(this) //LayoutManager controla como se van a ver las vistas
+        adapter = TaskAdapter(tasks)
+        rvTask.adapter = adapter
     }
 
     //Llama a los metodos que inicializan y controlan el evento
@@ -30,9 +41,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     //Añade una tarea
+    @SuppressLint("NotifyDataSetChanged")
     private fun addTask() {
         val taskToAdd:String = etTask.text.toString()
         tasks.add(taskToAdd)
+        adapter.notifyDataSetChanged() //Notifica que se han añadido nuevos valores
+        etTask.setText("")
     }
 
     //Inicializar las variables buscando por su id
