173d16eee0fdd524fa4cb5ade6ca68ead0a6f33d
Julio <juliogtrenard@gmail.com>
2024-10-23 10:18:33 +0200

    ADD xml de la TaskView

--- app/src/main/java/es/juliogtrenard/miprimeraapp/TaskViewHolder.kt
+++ app/src/main/java/es/juliogtrenard/miprimeraapp/TaskViewHolder.kt
@@ -1,8 +1,13 @@
 package es.juliogtrenard.miprimeraapp
 
 import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class TaskViewHolder(view: View):RecyclerView.ViewHolder(view) {
+    private val tvTask:TextView = view.findViewById(R.id.tvTask)
 
+    fun render(task:String) {
+        tvTask.text = task
+    }
 }
